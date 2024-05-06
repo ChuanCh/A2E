c977bcada1d675a08e972d56517c8532c8a350bd
ChuanCh <bfsuchc@gmail.com>
2024-05-06 22:58:44 +0200

    5.6

--- audio_process.py
+++ audio_process.py
@@ -1,15 +1,13 @@
 '''
-Pre process audio and EGG data
+Pre process audio and EGG data:
+Audio files move, rename, segment, check integrity
 Normalization
-Segmentation
 [Feature extraction]
 '''
 
 import os
-import librosa
-import numpy as np
 from pydub import AudioSegment, silence
-import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter, firwin, medfilt
 
 def move_audio_files(audio_dir, output_dir):
     '''
@@ -42,9 +40,6 @@ def rename(dir):
     # remove the string '_C_' from the file names
     for file in wav_files:
         os.rename(os.path.join(dir, file), os.path.join(dir, file.replace('_C_', '_')))
-
-import os
-from pydub import AudioSegment, silence
 
 def segment_audio(file_path, min_silence_len=200, silence_thresh=-60, target_length=10):
     # Load audio file
@@ -88,7 +83,74 @@ def process_folder(folder_path, output_dir_audio, output_dir_egg):
                 audio_seg.export(audio_output_path, format='wav')
                 egg_seg.export(egg_output_path, format='wav')
 
-audio_dir = r'F:\A2E git\A2E\audio\VRP'
-voice_dir = r'F:\A2E git\A2E\audio\VRP_segmented\Voice'
-egg_dir = r'F:\A2E git\A2E\audio\VRP_segmented\EGG'
-process_folder(audio_dir, voice_dir, egg_dir)
+def compute_total_duration(folder_path):
+    total_duration = 0
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.wav'):
+            file_path = os.path.join(folder_path, file_name)
+            audio = AudioSegment.from_wav(file_path)
+            total_duration += len(audio)
+    return total_duration / 1000
+
+def check_integrity(folder_path):
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.wav'):
+            file_path = os.path.join(folder_path, file_name)
+            audio = AudioSegment.from_wav(file_path)
+            if audio.channels != 1:
+                print(f"File {file_name} has {audio.channels} channels")
+            if audio.frame_rate != 16000:
+                print(f"File {file_name} has {audio.frame_rate} frame rate")
+            if audio.sample_width != 2:
+                print(f"File {file_name} has {audio.sample_width} sample width")
+
+
+def voice_preprocess(voice, sample_rate):
+    def butter_highpass(cutoff, sample_rate, order=2):
+        nyquist = 0.5 * sample_rate
+        normal_cutoff = cutoff / nyquist
+        b, a = butter(order, normal_cutoff, btype='high', analog=False)
+        return b, a
+
+    def highpass_filter(data, cutoff, sample_rate, order=2):
+        b, a = butter_highpass(cutoff, sample_rate, order=order)
+        y = lfilter(b, a, data)
+        return y
+
+    if voice is None or len(voice) == 0:
+        raise ValueError("Invalid voice data: Input is None or empty.")
+    
+    voice = highpass_filter(voice, 30, sample_rate)
+
+    return voice
+
+def egg_preprocess(egg, fs):
+    def process_EGG_signal(egg_signal, threshold_dB=-40, expansion_ratio=1/4):
+        # Apply a band-pass filter
+        filtered_signal = apply_band_pass_filter(egg_signal, fs)
+        
+        # Apply a nine-point running median filter
+        filtered_signal = medfilt(filtered_signal, kernel_size=9)
+
+        return filtered_signal
+
+    def apply_band_pass_filter(signal, sample_rate, low_cutoff=40, high_cutoff=10000, numtaps=255):
+        # Design the band-pass FIR filter
+        fir_coeff = firwin(numtaps, [low_cutoff, high_cutoff], pass_zero=False, fs=sample_rate, window='hamming')
+
+        # Apply the filter to the signal
+        filtered_signal = lfilter(fir_coeff, 1.0, signal)
+
+        return filtered_signal
+
+    if egg is not None and len(egg) > 0:
+        return process_EGG_signal(egg, fs)
+    else:
+        raise ValueError("Invalid EGG signal: Input is None or empty.")
+
+voice_dir = r'F:\audio\SRP_segmented\Voice'
+egg_dir = r'F:\audio\SRP_segmented\EGG'
+# process_folder(audio_dir, voice_dir, egg_dir)
+
+print(compute_total_duration(voice_dir))
+print(compute_total_duration(egg_dir))
