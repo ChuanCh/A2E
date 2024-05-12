967f011c3655274c695e786a8120d4d3343412f5
ChuanCh <bfsuchc@gmail.com>
2024-05-12 23:46:37 +0200

    SRP VRP separated

--- audio_process.py
+++ audio_process.py
@@ -41,7 +41,7 @@ def rename(dir):
     for file in wav_files:
         os.rename(os.path.join(dir, file), os.path.join(dir, file.replace('_C_', '_')))
 
-def segment_audio(file_path, min_silence_len=200, silence_thresh=-60, target_length=10):
+def segment_audio(file_path, min_silence_len=30, silence_thresh=-60, target_length=15):
     # Load audio file
     audio = AudioSegment.from_wav(file_path)
     audio_channel,egg_channel = audio.split_to_mono()
@@ -50,7 +50,7 @@ def segment_audio(file_path, min_silence_len=200, silence_thresh=-60, target_len
 
     # print total chunks time in seconds
     total_time = sum([len(chunk) for chunk in chunks]) / 1000
-    print(f"Total chunks time: {total_time} seconds")
+    # print(f"Total chunks time: {total_time} seconds")
 
 
     # Create segments around 10 seconds
@@ -59,12 +59,17 @@ def segment_audio(file_path, min_silence_len=200, silence_thresh=-60, target_len
     segment_end = 0
 
     for chunk in chunks:
-        current_length = segment_end - segment_start + len(chunk)
-        if current_length < target_duration:
-            segment_end += len(chunk)
-        else:
+        if chunk.duration_seconds > target_duration:
+            segment_end = segment_start + target_duration
             yield audio_channel[segment_start:segment_end], egg_channel[segment_start:segment_end]
             segment_start = segment_end
+        else:
+            current_length = segment_end - segment_start + len(chunk)
+            if current_length < target_duration:
+                segment_end += len(chunk)
+            else:
+                yield audio_channel[segment_start:segment_end], egg_channel[segment_start:segment_end]
+                segment_start = segment_end
 
 
 def process_folder(folder_path, output_dir_audio, output_dir_egg):
@@ -76,6 +81,9 @@ def process_folder(folder_path, output_dir_audio, output_dir_egg):
             for i, (audio_seg, egg_seg) in enumerate(segment_audio(file_path)):
                 audio_file_name = f"{base_name}_Voice_{i}.wav"  # Format the new file name
                 egg_file_name = f"{base_name}_EGG_{i}.wav"
+
+                if audio_seg.duration_seconds < 1:
+                    print(f"Segment {audio_file_name} is too short")
 
                 audio_output_path = os.path.join(output_dir_audio, audio_file_name)
                 egg_output_path = os.path.join(output_dir_egg, egg_file_name)
@@ -105,52 +113,68 @@ def check_integrity(folder_path):
                 print(f"File {file_name} has {audio.sample_width} sample width")
 
 
+def butter_highpass(cutoff, sample_rate, order=2):
+    nyquist = 0.5 * sample_rate
+    normal_cutoff = cutoff / nyquist
+    b, a = butter(order, normal_cutoff, btype='high', analog=False)
+    return b, a
+
+def highpass_filter(data, cutoff, sample_rate, order=2):
+    b, a = butter_highpass(cutoff, sample_rate, order=order)
+    y = lfilter(b, a, data)
+    return y
+
 def voice_preprocess(voice, sample_rate):
-    def butter_highpass(cutoff, sample_rate, order=2):
-        nyquist = 0.5 * sample_rate
-        normal_cutoff = cutoff / nyquist
-        b, a = butter(order, normal_cutoff, btype='high', analog=False)
-        return b, a
-
-    def highpass_filter(data, cutoff, sample_rate, order=2):
-        b, a = butter_highpass(cutoff, sample_rate, order=order)
-        y = lfilter(b, a, data)
-        return y
-
-    if voice is None or len(voice) == 0:
-        raise ValueError("Invalid voice data: Input is None or empty.")
-    
     voice = highpass_filter(voice, 30, sample_rate)
-
     return voice
 
-def egg_preprocess(egg, fs):
-    def process_EGG_signal(egg_signal, threshold_dB=-40, expansion_ratio=1/4):
-        # Apply a band-pass filter
-        filtered_signal = apply_band_pass_filter(egg_signal, fs)
-        
-        # Apply a nine-point running median filter
-        filtered_signal = medfilt(filtered_signal, kernel_size=9)
+def process_EGG_signal(egg_signal, sample_rate, threshold_dB=-40, expansion_ratio=1/4):
+    # high pass filter
+    filtered_signal = apply_high_pass_filter(egg_signal)
+    
+    # low pass filter
+    filtered_signal = apply_low_pass_filter(filtered_signal)
+    
+    # a nine-point running median filter
 
-        return filtered_signal
+    return filtered_signal
 
-    def apply_band_pass_filter(signal, sample_rate, low_cutoff=40, high_cutoff=10000, numtaps=255):
-        # Design the band-pass FIR filter
-        fir_coeff = firwin(numtaps, [low_cutoff, high_cutoff], pass_zero=False, fs=sample_rate, window='hamming')
+def apply_high_pass_filter(signal, sample_rate=44100, numtaps=1025, cutoff=80):
+    # Design the FIR filter
+    fir_coeff = firwin(numtaps, cutoff, pass_zero=False, fs=sample_rate, window='hamming')
 
-        # Apply the filter to the signal
-        filtered_signal = lfilter(fir_coeff, 1.0, signal)
+    # Apply the filter to the signal using lfilter, which applies the filter in a linear-phase manner
+    filtered_signal = lfilter(fir_coeff, 1.0, signal)
+    
+    return filtered_signal
 
-        return filtered_signal
+def apply_low_pass_filter(signal, sample_rate=44100, cutoff_hz=10000, numtaps=1025):
+    # Design the low-pass FIR filter with a cutoff of 10 kHz
+    fir_coeff = firwin(numtaps, cutoff_hz, fs=sample_rate, window='hamming', pass_zero=True)
 
-    if egg is not None and len(egg) > 0:
-        return process_EGG_signal(egg, fs)
-    else:
-        raise ValueError("Invalid EGG signal: Input is None or empty.")
+    # Apply the filter to the signal
+    filtered_signal = lfilter(fir_coeff, 1.0, signal)
 
-voice_dir = r'F:\audio\SRP_segmented\Voice'
-egg_dir = r'F:\audio\SRP_segmented\EGG'
-# process_folder(audio_dir, voice_dir, egg_dir)
+    return filtered_signal
 
-print(compute_total_duration(voice_dir))
-print(compute_total_duration(egg_dir))
+def delete_0s_audio(audio_dir, sr):
+    for file_name in os.listdir(audio_dir):
+        if file_name.endswith('.wav'):
+            file_path = os.path.join(audio_dir, file_name)
+            audio = AudioSegment.from_wav(file_path)
+            if len(audio) < sr:
+                os.remove(file_path)
+                print(f"File {file_name} deleted")
+
+def main():
+    folder_path = r'F:\audio\SRP'
+    output_dir_audio = r'F:\audio\SRP_segmented\Voice'
+    output_dir_egg = r'F:\audio\SRP_segmented\EGG'
+    if not os.path.exists(output_dir_audio):
+        os.makedirs(output_dir_audio)
+    if not os.path.exists(output_dir_egg):
+        os.makedirs(output_dir_egg)
+    process_folder(folder_path, output_dir_audio, output_dir_egg)
+
+if __name__ == '__main__':
+    main()
